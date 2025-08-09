4f43b296ba2ce4287f8c62d672bf7e852aa565df
cakmalik <hasinilmalik@gmail.com>
2025-08-09 07:35:13 +0700

    handle current host development vercel link

--- pages/pay.vue
+++ pages/pay.vue
@@ -1,28 +1,14 @@
 <template>
   <NuxtLayout name="public">
     <div class="max-w-md mx-auto text-center mt-20 space-y-4 space-x-2">
-      <USelectMenu
-        v-model="selectedHouse"
-        :items="houseOptions"
-        label="No Rumah"
-        placeholder="Pilih no rumah"
-        class="w-full sm:w-1/2 md:w-1/4"
-        :loading="isLoading"
-      />
+      <USelectMenu v-model="selectedHouse" :items="houseOptions" label="No Rumah" placeholder="Pilih no rumah"
+        class="w-full sm:w-1/2 md:w-1/4" :loading="isLoading" />
 
-      <UButton
-        :disabled="!selectedHouse || isProcessing"
-        :loading="isProcessing"
-        @click="makePayment"
-        color="primary"
-      >
+      <UButton :disabled="!selectedHouse || isProcessing" :loading="isProcessing" @click="makePayment" color="primary">
         Bayar Sekarang
       </UButton>
 
-      <div
-        v-if="result?.data?.data"
-        class="mt-6 text-center space-y-6 text-gray-700 dark:text-white"
-      >
+      <div v-if="result?.data?.data" class="mt-6 text-center space-y-6 text-gray-700 dark:text-white">
         <h2 class="text-xl font-semibold">
           {{ result.data.data.customer_name }}
         </h2>
@@ -46,27 +32,17 @@
         </h2>
 
         <!-- QR CODE -->
-        <img
-          :src="result.data.data.qr_url"
-          alt="QR Code Pembayaran"
-          ref="qrImage"
-          class="mx-auto w-60 h-60 rounded border"
-        />
+        <img :src="result.data.data.qr_url" alt="QR Code Pembayaran" ref="qrImage"
+          class="mx-auto w-60 h-60 rounded border" />
 
         <!-- BUTTON DOWNLOAD -->
         <UButton @click="downloadQr" color="gray"> Download QR Code </UButton>
 
         <!-- INSTRUCTIONS -->
-        <div
-          v-if="result.data.data.instructions?.length"
-          class="text-left max-w-md mx-auto space-y-4"
-        >
+        <div v-if="result.data.data.instructions?.length" class="text-left max-w-md mx-auto space-y-4">
           <h3 class="text-lg font-semibold">Instruksi Pembayaran</h3>
-          <div
-            v-for="(instruction, idx) in result.data.data.instructions"
-            :key="idx"
-            class="border rounded p-4 bg-gray-50"
-          >
+          <div v-for="(instruction, idx) in result.data.data.instructions" :key="idx"
+            class="border rounded p-4 bg-gray-50">
             <h4 class="font-bold mb-2 text-gray-800">
               {{ instruction.title }}
             </h4>
@@ -116,6 +92,8 @@ onMounted(async () => {
     const currentHost = window.location.host;
     const currentPath =
       window.location.pathname + window.location.search + window.location.hash;
+
+    if (currentHost == 'vba-007-hesa.vercel.app') return;
 
     // Redirect jika bukan dari domain utama
     if (currentHost !== "rt007.site") {
